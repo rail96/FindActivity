a7e73ff10d4ad05a28a334090553d9edf70f9632
Rail Latypov <v-ralaty@microsoft.com>
2020-12-13 00:46:22 +0300

    Added default values for user context and optional parameters for user and sql connection

--- FindActivity/Program.cs
+++ FindActivity/Program.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using FindActivity.Models;
+using FindActivity.Controllers;
 
 namespace FindActivity
 {
@@ -13,7 +16,23 @@ namespace FindActivity
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<UserContext>();
+                    SampleData.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
