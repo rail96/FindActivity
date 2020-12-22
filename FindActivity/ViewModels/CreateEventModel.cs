c4d38eadb6686301b9ad0012fff7f9cc3780f9b7
Rail Latypov <v-ralaty@microsoft.com>
2020-12-22 20:33:44 +0300

    Deleted id, addded some random id for events

--- FindActivity/ViewModels/CreateEventModel.cs
+++ FindActivity/ViewModels/CreateEventModel.cs
@@ -8,7 +8,6 @@ namespace FindActivity.ViewModels
 {
     public class CreateEventModel
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
         public string Description { get; set; }
